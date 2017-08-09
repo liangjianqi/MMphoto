d164ded714417913069d78cdc5bd0958a4ea8758
Domon <viplizhao@gmail.com>
2017-08-09 14:42:30 +0800

    Add home package

---
+++ app/src/main/java/domon/cn/mmphoto/home/HomeFragment.java
@@ -0,0 +1,8 @@
+package domon.cn.mmphoto.home;
+
+/**
+ * Created by Domon on 2017/8/9.
+ */
+
+public class HomeFragment {
+}
---
+++ app/src/main/java/domon/cn/mmphoto/home/HomePresenter.java
@@ -0,0 +1,15 @@
+package domon.cn.mmphoto.home;
+
+import domon.cn.mmphoto.BasePresenter;
+
+/**
+ * Created by Domon on 2017/8/9.
+ */
+
+public class HomePresenter implements BasePresenter {
+
+    @Override
+    public void start() {
+
+    }
+}
